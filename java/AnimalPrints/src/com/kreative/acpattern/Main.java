97d735cea062cfc6522a34b539d7e460097bff0d
Rebecca G. Bettencourt <beckiejo@gmail.com>
2021-05-20 19:29:46 -0700

    Dispatch to secret main methods

--- java/AnimalPrints/src/com/kreative/acpattern/Main.java
+++ java/AnimalPrints/src/com/kreative/acpattern/Main.java
@@ -2,6 +2,9 @@ package com.kreative.acpattern;
 
 import java.util.Arrays;
 import java.util.List;
+import com.kreative.acpattern.robot.PatternBot;
+import com.kreative.acpattern.robot.SwitchControl;
+import com.kreative.acpattern.robot.WishBot;
 
 public class Main {
 	public static void main(String[] args) {
@@ -10,13 +13,26 @@ public class Main {
 			return;
 		}
 		String verb = args[0].trim().toLowerCase();
-		if (verb.equals("convert")) {
-			ACConvert.main(subarray(args, 1));
-		} else if (verb.equals("view")) {
-			ACView.main(subarray(args, 1));
-		} else {
-			ACView.main(args);
-		}
+		if (verb.equals("help") || verb.equals("--help")) help();
+		else if (verb.equals("convert")) ACConvert.main(subarray(args, 1));
+		else if (verb.equals("view")) ACView.main(subarray(args, 1));
+		else if (verb.equals("acnhdebug")) ACNHDebug.main(subarray(args, 1));
+		else if (verb.equals("acnldebug")) ACNLDebug.main(subarray(args, 1));
+		else if (verb.equals("acqrdebug")) ACQRDebug.main(subarray(args, 1));
+		else if (verb.equals("acwwdebug")) ACWWDebug.main(subarray(args, 1));
+		else if (verb.equals("switchcontrol")) SwitchControl.main(subarray(args, 1));
+		else if (verb.equals("patternbot")) PatternBot.main(subarray(args, 1));
+		else if (verb.equals("wishbot")) WishBot.main(subarray(args, 1));
+		else ACView.main(args);
+	}
+	
+	private static void help() {
+		System.out.println();
+		System.out.println("Animal Prints - View and convert Animal Crossing design patterns.");
+		System.out.println();
+		System.out.println("animalprints convert <options> <files>");
+		System.out.println("animalprints view <files>");
+		System.out.println();
 	}
 	
 	private static String[] subarray(String[] a, int i) {
--- java/AnimalPrints/src/com/kreative/acpattern/robot/WishBot.java
+++ java/AnimalPrints/src/com/kreative/acpattern/robot/WishBot.java
@@ -2,7 +2,7 @@ package com.kreative.acpattern.robot;
 
 import java.io.IOException;
 import java.util.List;
-
+import java.util.Scanner;
 import com.kreative.acpattern.robot.SwitchController.Button;
 import com.kreative.acpattern.robot.SwitchController.DPad;
 
@@ -39,30 +39,53 @@ public class WishBot {
 		SwitchController ctrl = new SwitchController(port.getOutputStream());
 		ctrl.sleep(1000);
 		
-		// Deselect tool
-		ctrl.clickDPad(DPad.DOWN);
-		ctrl.clickDPad(DPad.DOWN);
-		ctrl.clickDPad(DPad.DOWN);
-		ctrl.clickDPad(DPad.DOWN);
-		
-		// Highest camera angle
-		ctrl.nudgeRightStick(0, -1);
-		ctrl.nudgeRightStick(0, -1);
-		ctrl.nudgeRightStick(0, -1);
-		ctrl.nudgeRightStick(0, -1);
-		
-		// Mash A
-		while (!Thread.interrupted()) {
-			ctrl.pressButton(Button.A);
-			ctrl.sleep(500);
-			ctrl.releaseButton(Button.A);
-			ctrl.sleep(500);
+		// Control loop
+		Scanner scan = new Scanner(System.in);
+		WishThread thread = null;
+		for (;;) {
+			System.out.println("Press enter to start wishing or ^D to exit.");
+			if (scan.hasNextLine()) scan.nextLine(); else break;
+			thread = new WishThread(ctrl); thread.start();
+			System.out.println("Press enter to stop wishing or ^D to exit.");
+			if (scan.hasNextLine()) scan.nextLine(); else break;
+			thread.interrupt(); thread = null;
 		}
+		if (thread != null) thread.interrupt();
+		scan.close();
 		
 		// Close the serial port
 		ctrl.sleep(1000);
 		try { port.close(); }
 		catch (IOException e) {}
 		System.exit(0);
+	}
+	
+	public static final class WishThread extends Thread {
+		private final SwitchController ctrl;
+		public WishThread(SwitchController ctrl) {
+			this.ctrl = ctrl;
+		}
+		@Override
+		public void run() {
+			// Deselect tool
+			ctrl.clickDPad(DPad.DOWN);
+			ctrl.clickDPad(DPad.DOWN);
+			ctrl.clickDPad(DPad.DOWN);
+			ctrl.clickDPad(DPad.DOWN);
+			
+			// Highest camera angle
+			ctrl.nudgeRightStick(0, -1);
+			ctrl.nudgeRightStick(0, -1);
+			ctrl.nudgeRightStick(0, -1);
+			ctrl.nudgeRightStick(0, -1);
+			
+			// Mash A
+			while (!Thread.interrupted()) {
+				ctrl.pressButton(Button.A);
+				ctrl.sleep(500);
+				ctrl.releaseButton(Button.A);
+				ctrl.sleep(500);
+			}
+		}
 	}
 }
